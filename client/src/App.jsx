9cf3c08a088a46002e811cda1134c2a6eaeab32b
faishalam <faishalabdulmajid@gmail.com>
2023-12-19 19:47:40 +0700

    board

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ChessboardComponent from './components/Chessboard';
+import PlayRandomMoveEngine from './components/Chessboard';
+
+const App = () => {
+  return (
+    <div className="text-center my-8">
+      <h1 className="text-2xl font-bold mb-4">Chess Game</h1>
+      <div className="inline-block shadow-md rounded-xl overflow-hidden mt-10">
+        <ChessboardComponent />
+      </div>
+    </div>
+  );
+};
+
+export default App;
